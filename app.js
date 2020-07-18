0def9da2c2dcb8766b0adffa9841cb97e65e5ad3
vinisimoes <vini.lopes.simoes@gmail.com>
2020-07-17 23:53:54 -0300

    Tratamento de erros adicionado

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+
+// QUANDO NAO ENCONTRA ROTA
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
+module.exports = app;
---
+++ routes/produtos.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+// RETORNA TODOS OS PRODUTOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Retorna todos os produtos'
+    });
+});
+
+// INSERE UM PRODUTO
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'O produto foi criado'
+    });    
+});
+
+// RETORNA OS DADOS DE UM PRODUTO 
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto;
+    var msg;
+
+    if (id === 'especial') {
+        msg = 'Você descobriu o produto especial';
+    } else {
+        msg = 'Retorna os dados de um produto';
+    }
+
+    res.status(200).send({
+        mensagem: msg,
+        id: id
+    });    
+
+});
+
+// ALTERA UM PRODUTO
+router.patch('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Produto alterado'
+    });    
+});
+
+// EXCLUI UM PRODUTO
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Produto excluído'
+    }); 
+});
+
+module.exports = router;
